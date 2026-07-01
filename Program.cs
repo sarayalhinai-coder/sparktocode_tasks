15c014afcdd84469e816bef092f7bc863b697cc8
sara <sara.y.alhinai@gmail.com>
2026-07-01 18:25:58 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sparktocode_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
