17fe030cd6c694eb9b017d09a31ba9dd5624edf1
sara <sara.y.alhinai@gmail.com>
2026-07-02 09:38:01 +0400

    task 9

---
+++ (1-7)task 9/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _1_7_task_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number from 1 to 7: ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("monday");
+                    break;
+                case 3:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("friday");
+                    break;
+                case 7:
+                    Console.WriteLine("saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
+
+        }
+    }
+}
