943f7b0aadbe0d81254b63c4d49f2d47c8b50afe
sara <sara.y.alhinai@gmail.com>
2026-07-02 09:18:01 +0400

    task 6

---
+++ (1-7)task 6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1_7_task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
