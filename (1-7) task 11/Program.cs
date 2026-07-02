916cbc08b5851c1ab42008c7c73c218aecc9d816
sara <sara.y.alhinai@gmail.com>
2026-07-02 09:53:27 +0400

    task 11

---
+++ (1-7) task 11/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _1_7__task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your age:");
+            int age11 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter your income:");
+            float income = float.Parse(Console.ReadLine());
+            Console.WriteLine("do you have any existing loan (yes/no):");
+            string loanInput = Console.ReadLine().ToLower();
+            bool loan = (loanInput == "yes");
+
+            if (age11 >= 21 && age11 <= 60 && income >= 400 && !loan)
+            {
+                Console.WriteLine("you are eligible for loan");
+            }
+            else
+            {
+                if (age11 < 21 || age11 > 60)
+                {
+                    Console.WriteLine("age out of range");
+                }
+                else if (income < 400)
+                {
+                    Console.WriteLine("income too low ");
+                }
+                else if (loan == true)
+                {
+                    Console.WriteLine("has existing loan");
+                }
+            }
+
+        }
+    }
+}
