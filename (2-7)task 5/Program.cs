b67d3a776e96f72861154ac1724236cd0ff34e39
sara <sara.y.alhinai@gmail.com>
2026-07-02 12:09:24 +0400

    task 5

---
+++ (2-7)task 5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2_7_task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 42;
+            int counter = 0;
+            int guess;
+            do
+            {
+                Console.WriteLine("enter guess:");
+                guess = int.Parse(Console.ReadLine());
+                counter = counter + 1;
+                if (x > guess)
+                {
+                    Console.WriteLine("too low");
+                }
+                else if (x < guess)
+                {
+                    Console.WriteLine("too high");
+                }
+            } while (x != guess);
+
+            Console.WriteLine("correct , it took "+ counter+" attempts");
+        }
+    }
+}
