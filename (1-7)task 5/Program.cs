5723c0acc167ba39d6012ce1526f003ff98711f6
sara <sara.y.alhinai@gmail.com>
2026-07-02 09:12:24 +0400

    task 5

---
+++ (1-7)task 5/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1_7_task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter grade character:");
+            Console.WriteLine("1.A");
+            Console.WriteLine("2.B");
+            Console.WriteLine("3.C");
+            Console.WriteLine("4.D");
+            Console.WriteLine("5.F");
+            int grade = int.Parse(Console.ReadLine());
+            switch (grade)
+            {
+                case 1:
+                    Console.WriteLine("Excellent");
+                    break;
+                case 2:
+                    Console.WriteLine("Very Good");
+                    break;
+                case 3:
+                    Console.WriteLine("Good");
+                    break;
+                case 4:
+                    Console.WriteLine("Pass");
+                    break;
+                case 5:
+                    Console.WriteLine("Fail");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+
+        }
+    }
+}
