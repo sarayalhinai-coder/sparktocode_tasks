10b9a21714f31d28bd2f5ec5a8b483d63f655517
sara <sara.y.alhinai@gmail.com>
2026-07-05 14:58:54 +0400

    create tasks file

---
+++ (5-7)CsharpFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5_7_CsharpFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
