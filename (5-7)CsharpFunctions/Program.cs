417162e0ca0b4aa3d9727232a9b5d930f619ce66
sara <sara.y.alhinai@gmail.com>
2026-07-05 15:08:54 +0400

    task 1

--- (5-7)CsharpFunctions/Program.cs
+++ (5-7)CsharpFunctions/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //task 1  
+            Console.WriteLine("enter first number :");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = Math.Abs(num1 - num2);
+            Console.WriteLine("final positive difference= "+result);
+
+
         }
     }
 }
