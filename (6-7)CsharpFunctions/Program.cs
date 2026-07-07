b15d13aebbb7b0c1d3b83ee8ad4e3c08565d1920
sara <sara.y.alhinai@gmail.com>
2026-07-07 07:52:08 +0400

    task 2

--- (6-7)CsharpFunctions/Program.cs
+++ (6-7)CsharpFunctions/Program.cs
@@ -7,12 +7,24 @@
         {
             Console.WriteLine("welcome " + name);
         }
+
+        //task 2 function
+        static int Square(int x) 
+        {
+            int sq = x*x ;
+            return sq;
+        }
         static void Main(string[] args)
         {
             //task 1 main 
             Console.WriteLine("enter your name ");
             string name=Console.ReadLine();
             PrintWelcome(name);
+
+            //task 2 main 
+            Console.WriteLine("enter number:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("the square of "+ x + " = "+Square(x));
         }
     }
 }
