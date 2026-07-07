acab2159fe7c01f313437678eef15ecb77d24bf1
sara <sara.y.alhinai@gmail.com>
2026-07-07 07:44:22 +0400

    task 1

---
+++ (6-7)CsharpFunctions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _6_7_CsharpFunctions
+{
+    internal class Program
+    {
+        //task 1 function 
+        static void PrintWelcome(string name)
+        {
+            Console.WriteLine("welcome " + name);
+        }
+        static void Main(string[] args)
+        {
+            //task 1 main 
+            Console.WriteLine("enter your name ");
+            string name=Console.ReadLine();
+            PrintWelcome(name);
+        }
+    }
+}
