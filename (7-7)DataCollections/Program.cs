50b7466bd01e33f4e8c56c6667c137def5efcace
sara <sara.y.alhinai@gmail.com>
2026-07-08 09:08:15 +0400

    task 2

--- (7-7)DataCollections/Program.cs
+++ (7-7)DataCollections/Program.cs
@@ -11,11 +11,26 @@
                 Console.WriteLine("enter grade : ");
                 grades[i] = int.Parse(Console.ReadLine());
             }
-            Console.WriteLine("the grades : ");
+            Console.WriteLine("--the grades--");
             foreach (int grade in grades)
             {
                 Console.WriteLine(grade);
             }
+
+            //task 2 
+            List<string> tasks=new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter task : ");
+                string task = Console.ReadLine();
+                tasks.Add(task);
+            }
+            Console.WriteLine("-- tasks list ---");
+            foreach(string a in tasks)
+            {
+                Console.WriteLine(a);
+            }
+
         }
     }
 }
