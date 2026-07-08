c1dece36ce8cbbf725e92cc0e92ab5c008e92441
sara <sara.y.alhinai@gmail.com>
2026-07-08 08:59:19 +0400

    task 1

---
+++ (7-7)DataCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _7_7_DataCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1 
+            int[] grades=new int[5];
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("enter grade : ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("the grades : ");
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
