f976c59d3958acb68bacc83ae8ad93a50fd2be76
sara <sara.y.alhinai@gmail.com>
2026-07-12 10:13:35 +0400

    BankAccount class

---
+++ (12-7)OOP1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _12_7_OOP1
+{
+
+    public class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public string? HolderName { get; set; }
+        public double Balance { get; set; }
+        
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            SendEmail();
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine("ERROR: withdraw amount exceeds account balance");
+                return;
+            }
+            else
+            {
+                Balance -= amount;
+            }
+            SendEmail();
+        }
+
+        public double CheckBalance()
+        {
+            PrintInformation();
+            Console.Write("current Balance: ");
+            return Balance;
+        }
+        private void PrintInformation()
+        {
+            Console.WriteLine("Holder name: "+HolderName);
+            Console.WriteLine("Balance: "+Balance);
+        }
+
+        private void SendEmail()
+        {
+            Console.WriteLine("Email has been sent");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
