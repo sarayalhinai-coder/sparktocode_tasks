663ca4b200e05bc018299ce514ffb7551f23fb99
sara <sara.y.alhinai@gmail.com>
2026-07-14 12:51:21 +0400

    Guest class

--- (14-7)HotelManagementSystem/Guest.cs
+++ (14-7)HotelManagementSystem/Guest.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace _14_7_HotelManagementSystem
 {
-    internal class Guest
+    public class Guest
     {
+        public int guestId {  get; set; }
+        public string guestName{ get; set; }
+        public string roomNumber { get; set; }
+        public string checkInDate {  get; set; }
+        public int totalNights { get; set; }
+
+
+        public Guest(int Id, string Name, string roomNO, string Date, int Nights)
+        {
+            Id = guestId;
+            Name = guestName;
+            roomNO = roomNumber;
+            Date = checkInDate;
+            Nights = totalNights;
+
+        }
+
+        public void displayGuest()
+        {
+            Console.WriteLine("Guest ID: "+guestId);
+            Console.WriteLine("Guest Name: " +guestName);
+            Console.WriteLine("Room Number: " + roomNumber);
+            Console.WriteLine("Chech in Date: " + checkInDate);
+            Console.WriteLine("Total Nights: " + totalNights);
+        }
+
+        public double CalculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * totalNights;
+        }
     }
 }
