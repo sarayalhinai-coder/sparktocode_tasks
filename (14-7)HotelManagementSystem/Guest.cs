b529e00c3756e1177e48fcdd61b7e496310f2fe8
sara <sara.y.alhinai@gmail.com>
2026-07-14 11:56:30 +0400

    files created

---
+++ (14-7)HotelManagementSystem/Guest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14_7_HotelManagementSystem
+{
+    internal class Guest
+    {
+    }
+}
---
+++ (14-7)HotelManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14_7_HotelManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
