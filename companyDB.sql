ec2af6a18b4c2dff6730f322265a1f9fadf21ade
sara <sara.y.alhinai@gmail.com>
2026-07-21 15:29:06 +0400

    tables with fix

--- companyDB.sql
+++ companyDB.sql
@@ -2,24 +2,24 @@ CREATE DATABASE CompanyDB; GO; USE CompanyDB;
 
 CREATE TABLE Department(
 Dnumber INT NOT NULL PRIMARY KEY ,
-Dname VARCHAR NOT NULL UNIQUE ,
+Dname VARCHAR(15) NOT NULL UNIQUE ,
 NumberOfEmployees INT , 
-Mgr_ssn CHAR ,
+Mgr_ssn CHAR(9) NOT NULL,
 Mgr_start_date DATE ,
 );
 
 
 CREATE TABLE Employee (
-Ssn CHAR PRIMARY KEY ,
-Fname VARCHAR NOT NULL ,
-Minit CHAR NULL ,
-Lname VARCHAR NOT NULL , 
-Address VARCHAR ,
-Sex CHAR CHECK (Sex IN ('M','F')) ,--make sure the entery is correct 
+Ssn CHAR(9) PRIMARY KEY ,
+Fname VARCHAR(15) NOT NULL ,
+Minit CHAR(1) NULL ,
+Lname VARCHAR(15) NOT NULL , 
+Address VARCHAR(50),
+Sex CHAR(1) CHECK (Sex IN ('M','F')) ,--make sure the entery is correct 
 Bdate DATE ,
-Salary DECIMAL CHECK(Salary>0),--no neg numbers or 0
-Super_ssn CHAR ,
-Dno INT , 
+Salary DECIMAL(10,2) CHECK(Salary>0),--no neg numbers or 0
+Super_ssn CHAR(9),
+Dno INT NOT NULL , 
 CONSTRAINT FK_Employee_SuperSSN FOREIGN KEY(Super_ssn) REFERENCES Employee(Ssn) ,
 CONSTRAINT FK_Employee_Department  FOREIGN KEY(Dno) REFERENCES Department(Dnumber)
 );
@@ -31,33 +31,34 @@ FOREIGN KEY (Mgr_ssn) REFERENCES Employee(Ssn);
 CREATE TABLE Dept_locations(
 Dnumber INT NOT NULL ,
 CONSTRAINT FK_locations_Department  FOREIGN KEY(Dnumber) REFERENCES Department(Dnumber),
-Dlocation VARCHAR NOT NULL ,
+Dlocation VARCHAR(15) NOT NULL ,
 PRIMARY KEY (Dnumber, Dlocation),
 );
 
 CREATE TABLE Project (
-Pname VARCHAR NOT NULL UNIQUE , 
-Pnumber INT NOT NULL UNIQUE,
-PRIMARY KEY (Pname, Pnumber),
-Plocation VARCHAR , 
+Pname VARCHAR(15) NOT NULL UNIQUE , 
+Pnumber INT NOT NULL PRIMARY KEY  ,
+Plocation VARCHAR(15) , 
 Dnum INT NOT NULL , 
 CONSTRAINT FK_project_Department  FOREIGN KEY(Dnum) REFERENCES Department(Dnumber),
 );
 
 CREATE TABLE Works_on(
-Essn CHAR NOT NULL , 
+Essn CHAR(9) NOT NULL , 
 CONSTRAINT FK_Employee_SSN FOREIGN KEY(Essn) REFERENCES Employee(Ssn),
 Pno INT NOT NULL ,
 CONSTRAINT FK_project_no FOREIGN KEY(Pno) REFERENCES Project(Pnumber),
-Hours	DECIMAL CHECK (Hours > 0),
+PRIMARY KEY (Essn , Pno),
+Hours	DECIMAL(3,1) CHECK (Hours > 0),
+
 );
 
 CREATE TABLE Dependent(
-Dependent_name 	VARCHAR NOT NULL,
-Essn CHAR NOT NULL , 
+Dependent_name 	VARCHAR(15) NOT NULL,
+Essn CHAR(9) NOT NULL , 
 PRIMARY KEY (Dependent_name,Essn ),
 CONSTRAINT FK_Employee_Dep_SSN FOREIGN KEY(Essn) REFERENCES Employee(Ssn),
-Sex CHAR CHECK (Sex IN ('M','F')) ,
+Sex CHAR(1) CHECK (Sex IN ('M','F')) ,
 Bdate DATE ,
-Relationship VARCHAR,
+Relationship VARCHAR(15),
 );
