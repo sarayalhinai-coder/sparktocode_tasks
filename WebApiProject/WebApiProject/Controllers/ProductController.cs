30cb0295ea8799008bdf3462df5ff835f9f3761d
sara <sara.y.alhinai@gmail.com>
2026-07-29 19:41:59 +0400

    first part

---
+++ WebApiProject/WebApiProject/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using WebApiProject.Models;
+
+namespace WebApiProject.Controllers
+{
+    public class ProductController
+    {
+        private ProjectCantext context;
+
+        public ProductController(ProjectCantext _context)
+        {
+            context = _context;
+        }
+
+        public void AddProduct(Product p)
+        {
+            context.Products.Add(p);
+            context.SaveChanges();
+            
+        }
+
+        public void RemoveProduct(int id) 
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (p == null)
+            {
+                
+            }
+            else
+            {
+                context.Products.Remove(p);
+                context.SaveChanges();
+                
+            }
+        }
+    }
+}
